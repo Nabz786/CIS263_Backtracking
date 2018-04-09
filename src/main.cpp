e69ff9b59eb558d579df600071b2e96fbf0730da
Nabeel Vali <valin@eos24.cis.gvsu.edu>
2018-04-09 11:22:50 -0400

    Created main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <string>
+
+
+/**
+ *This file houses the back tracking algorithm to 
+ *sort and creat a new sudoku board
+ *
+ *@author Nabeel Vali
+ */
+int main(int argc, char ** argv){
+
+
+
+
+}
